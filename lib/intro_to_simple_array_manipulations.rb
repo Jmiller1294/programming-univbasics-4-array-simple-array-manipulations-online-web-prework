bd96250a92413768647e762dad21907569c7dc59
Justin Miller <jayman1294@gmail.com>
2019-08-24 23:05:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,7 +52,7 @@ def using_flatten(array)
 end
 
 def using_delete(array,element)
-  instructors = 
+  instructors = instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
 end
  
