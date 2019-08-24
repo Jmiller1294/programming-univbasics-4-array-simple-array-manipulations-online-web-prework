4655f6de86933d84d7c186193459d17bb760580f
Justin Miller <jayman1294@gmail.com>
2019-08-24 22:32:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -47,8 +47,6 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  instruments.flatten
 end
 
 def using_delete(array,element)
