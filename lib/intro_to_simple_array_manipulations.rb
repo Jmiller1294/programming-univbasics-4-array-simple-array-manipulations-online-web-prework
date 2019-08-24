a2bee204540f1c07741110e4f77498c38a4d3990
Justin Miller <jayman1294@gmail.com>
2019-08-24 21:30:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -41,7 +41,7 @@ def using_insert(array,element)
   @list_of_programming_languages.insert(4,@another_language)
 end
 
-def using_uniq
+def using_uniq(array)
   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
   haircuts.uniq
 end
