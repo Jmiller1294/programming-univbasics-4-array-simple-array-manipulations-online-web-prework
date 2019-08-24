b1e76dd7ec29c79c127ed96e3eeb1cf9e5f12c1f
Justin Miller <jayman1294@gmail.com>
2019-08-24 23:08:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,7 +53,7 @@ end
 
 def using_delete(array,element)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  
+  instructors.delete("Steven")
 end
  
 def using_delete_at(array,element)
